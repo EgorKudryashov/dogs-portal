4c26e9b1e86a8167c061785d2386dc1f7b715b8d
EgorKudryashov <70726535+EgorKudryashov@users.noreply.github.com>
2021-11-18 00:36:45 +0300

    Начало работы. Создание пустого сайта
    - Создание пустого сайта
    - Добавление навигационной панели

---
+++ frontend_dogs/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+import Navbar from "./component/UI/Navbar/navbar";
+import PageRouter from "./component/PageRouter";
+
+
+function App() {
+
+  return (
+      <BrowserRouter>
+          <Navbar/>
+          <PageRouter/>
+
+      </BrowserRouter>
+  );
+}
+
+export default App;
