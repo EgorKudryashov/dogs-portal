63502694b7a830647d1814ac86caf41c3a30a71c
EgorKudryashov <70726535+EgorKudryashov@users.noreply.github.com>
2021-12-18 15:55:07 +0300

    Добавление функционала на панель на странице public
    - Работающий поиск
    - Сортировка по лайкам

--- dogs_app/frontend_dogs/src/component/UI/Search/Search.jsx
+++ dogs_app/frontend_dogs/src/component/UI/Search/Search.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import classes from './Search.module.css'
+import {GetAllBreedsSorted} from "../../../api/GET";
+import {SearchBreed} from "../../../api/POST";
 
 
-const Search = () => {
+const Search = ({setInfo, setTotalInfo, limit,setPage, isLoading}) => {
+
+    const SortedByLikes = async ()=>{
+        await isLoading(false);
+        setPage(1);
+        await GetAllBreedsSorted(setInfo,setTotalInfo,limit)
+        await isLoading(true);
+    }
+
+    const SearchInformation= async ()=>{
+        let breed = document.getElementById("search").value;
+        await isLoading(false);
+        await SearchBreed(setInfo, breed, setTotalInfo, limit)
+        setPage(1);
+        await isLoading(true);
+    }
 
     return (
         <div className={classes.search}>
-            <input id="search" className={classes.search__item} style={{marginLeft: '35%'}} placeholder="  Введите название породы..." />
-            <button className={classes.search__button}>Поиск</button>
+            <button
+                className={classes.search__sort__button} style={{marginLeft: '10%'}}
+                onClick={SortedByLikes}>
+                Отсортировать по лайкам
+            </button>
+
+            <input id="search" className={classes.search__item} style={{marginLeft: '10%'}} placeholder="  Введите название породы..." />
+
+            <button
+                className={classes.search__button}
+                onClick={SearchInformation}>
+                Поиск
+            </button>
         </div>
     );
 };
